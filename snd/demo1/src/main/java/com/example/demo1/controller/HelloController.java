b40f28f2111d0f3b1a34cd7b25d44e93874c2bdc
zwb <zwb@gmail.com>
2021-01-09 13:02:10 +0800

    create tir

--- snd/demo1/src/main/java/com/example/demo1/controller/HelloController.java
+++ snd/demo1/src/main/java/com/example/demo1/controller/HelloController.java
@@ -1,11 +1,9 @@
 package com.example.demo1.controller;
 
-import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller("/hello")
+@RestController("/hello")
 public class HelloController {
 
 	@RequestMapping("/")
